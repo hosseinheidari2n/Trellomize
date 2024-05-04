39dc16f9f8f0c7acf5e731af8874fda99525ded6
hosseinheidari2n <92270802+hosseinheidari2n@users.noreply.github.com>
2024-05-04 20:00:05 +0330

    add project & tasks

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
+import datetime
 import hashlib
 import os
+import uuid
+from enum import Enum
 
 
 class Client:
@@ -175,6 +178,39 @@ def CalculateHash(input):
 def SecondCommandList():
     print("1. Create Project")
     print("2. Show Projects")
+
+
+class Project:
+    def __init__(self):
+        self.Id = str(uuid.uuid4()) # Generate a unique identifier
+        self.Title = ""
+        self.Members = []
+        self.Tasks = []
+        self.Leader = ""
+
+
+class Priority(Enum):
+    Low = 1
+    Medium = 2
+    High = 3
+
+class Status(Enum):
+    Backlog = 1
+    InProgress = 2
+    Completed = 3
+
+class Task:
+    def __init__(self):
+        self.Id = str(uuid.uuid4()) # Generate a unique identifier
+        self.Title = ""
+        self.Description = ""
+        self.StartDate = datetime.datetime.now()
+        self.EndDate = datetime.datetime.now() + datetime.timedelta(hours=24)
+        self.Assignees = []
+        self.Priority = Priority.Low # Default value for priority
+        self.Status = Status.Backlog # Default value for status
+        self.History = []
+        self.Comments = []
 
 if __name__ == "__main__":
     FirstCommandList()
